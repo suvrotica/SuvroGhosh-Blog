151d15c266f9cef427a9e79601b74dadccecc7d4
suvrowebthree <suvro.ghosh.contact@gmail.com>
2023-12-29 07:23:31 +0530

    Refactor Hero component and import global styles

--- src/lib/component/Hero.svelte
+++ src/lib/component/Hero.svelte
@@ -1,18 +1,13 @@
 <script>
 	import Carousal from './Carousal.svelte';
+	import { p_style, a_style, h1_style } from '$lib/global';
 </script>
 
 <section class="bg-white dark:bg-neutral-900">
 	<div class="mx-auto grid max-w-screen-xl px-4 py-8 lg:grid-cols-12 lg:gap-8 lg:py-16 xl:gap-0">
 		<div class="mr-auto place-self-center lg:col-span-7">
-			<h1
-				class="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white"
-			>
-				A Quixotic Blog
-			</h1>
-			<p
-				class="mb-6 max-w-2xl text-justify font-light text-neutral-500 md:text-lg lg:mb-8 lg:text-xl dark:text-neutral-400"
-			>
+			<h1 class={h1_style}>A Quixotic Blog</h1>
+			<p class={p_style}>
 				Although flippant, this blog is a scratchpad of thoughts, a much-sought-after extension of
 				my embarrassing tangle of nerves, unrelated fancies and figments strewn around and rattling
 				loose in it, collectibles from the waste basket scraps that should have seen a shredder, or,
@@ -25,21 +20,10 @@
 				android or an amateur, a ponderer or a pragmatist, join me in the infinite mosaic of
 				collective nonsense we can explore.
 			</p>
-			<a
-				href="/latest-post"
-				class="bg-primary-700 hover:bg-primary-800 focus:ring-primary-300 dark:focus:ring-primary-900 mr-3 inline-flex items-center justify-center rounded-lg px-5 py-3 text-center text-base font-medium text-white focus:ring-4"
-			>
-				Read Latest Post
-				<!-- Icon or SVG -->
-			</a>
-			<a
-				href="/about-me"
-				class="inline-flex items-center justify-center rounded-lg border border-neutral-300 px-5 py-3 text-center text-base font-medium text-neutral-900 hover:bg-neutral-100 focus:ring-4 focus:ring-neutral-100 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:ring-neutral-800"
-			>
-				Learn More About Me
-			</a>
+			<a href="/latest-post" class={a_style}> Latest Post </a>
+			<a href="/about-me" class={a_style}> About Me </a>
 		</div>
-		<div class="hidden lg:col-span-5 lg:mt-0 lg:flex">
+		<div class="display:block lg:col-span-5 lg:mt-0 lg:flex">
 			<Carousal />
 		</div>
 	</div>
---
+++ src/lib/global.ts
@@ -0,0 +1,6 @@
+export const p_style =
+	'mb-6 max-w-2xl text-justify font-light text-neutral-500 md:text-lg lg:mb-8 lg:text-xl dark:text-neutral-400';
+export const a_style =
+	'inline-flex items-center justify-center rounded-lg border border-neutral-300 px-5 py-3 text-center text-base font-medium text-neutral-900 hover:bg-neutral-100 focus:ring-4 focus:ring-neutral-100 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:ring-neutral-800';
+export const h1_style =
+	'mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white';
