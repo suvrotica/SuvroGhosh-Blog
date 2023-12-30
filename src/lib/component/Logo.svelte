49e58e7d282677a9034e57845e448553faf39fe9
suvrowebthree <suvro.ghosh.contact@gmail.com>
2023-12-30 19:09:35 +0530

    Update Logo styling

--- src/lib/component/Logo.svelte
+++ src/lib/component/Logo.svelte
@@ -1,11 +1,11 @@
 <a href="./" class="flex items-center">
 	<span
-		class="self-center whitespace-nowrap bg-white text-xl font-thin text-black dark:bg-black dark:text-white"
+		class="self-center whitespace-nowrap bg-white text-sm font-thin text-black dark:bg-neutral-900 dark:text-white"
 		>&nbsp;SUVRO GHOSH&nbsp;
 	</span>
 
 	<span
-		class="self-center whitespace-nowrap bg-black text-xl font-semibold text-white dark:bg-white dark:text-black"
+		class="self-center whitespace-nowrap bg-neutral-900 text-sm font-semibold text-white dark:bg-white dark:text-black"
 		><pre> blog </pre>
 	</span>
 </a>
