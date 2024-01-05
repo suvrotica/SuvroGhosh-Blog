40cbf5b6d75ef5f4ad0536f535461649e88df814
suvrowebthree <suvro.ghosh.contact@gmail.com>
2024-01-06 00:56:05 +0530

    Add emojis and update error page layout

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import { emojis } from '$lib/emojis.js';
 </script>
 
 <section class="bg-white dark:bg-neutral-900">
@@ -8,31 +9,26 @@
 			<h1
 				class="mb-4 text-7xl font-extrabold tracking-tight text-neutral-600 lg:text-9xl dark:text-neutral-500"
 			>
-				<h1>{$page.url.pathname}</h1>
+				{$page.url.pathname}
 			</h1>
-			<h2
-				class="mb-4 text-2xl font-bold tracking-tight text-neutral-900 md:text-4xl dark:text-white"
-			>
-				{$page.status}
-			</h2>
-			<h3
-				class="text-1xl mb-4 font-bold tracking-tight text-neutral-900 md:text-4xl dark:text-white"
-			>
-				{$page.error?.message}
-			</h3>
-			<p
-				class="mb-4 text-3xl font-bold tracking-tight text-neutral-900 md:text-4xl dark:text-white"
-			>
-				Missing Page
-			</p>
-			<p class="mb-4 text-lg font-light text-neutral-500 dark:text-neutral-400">
-				Sorry, we can't find that page. You'll find lots to explore on the home page.
-			</p>
-			<a
-				href="/"
-				class="my-4 inline-flex rounded-lg bg-neutral-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-neutral-800 focus:outline-none focus:ring-4 focus:ring-neutral-300 dark:focus:ring-neutral-900"
-				>Back to Homepage</a
-			>
+			<div>
+				<p class="mb-4 text-8xl font-bold tracking-tight text-neutral-900 dark:text-white">
+					{emojis[$page.status] ?? emojis[500]}
+				</p>
+				<p
+					class="mb-4 text-2xl font-bold tracking-tight text-neutral-900 md:text-4xl dark:text-white"
+				>
+					{$page.status}
+					({$page.error?.message})
+				</p>
+
+				<a
+					href="/"
+					class="my-4 inline-flex rounded-lg bg-neutral-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-neutral-800 focus:outline-none focus:ring-4 focus:ring-neutral-300 dark:focus:ring-neutral-900"
+				>
+					Back to Homepage
+				</a>
+			</div>
 		</div>
 	</div>
 </section>
