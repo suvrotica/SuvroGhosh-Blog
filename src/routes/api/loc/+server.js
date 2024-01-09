3a231a6873dd651ad30fddaeda0e9bdf25c19801
suvrowebthree <suvro.ghosh.contact@gmail.com>
2024-01-09 05:48:13 +0530

    Add request headers to GET endpoint

--- src/routes/api/loc/+server.js
+++ src/routes/api/loc/+server.js
@@ -1,11 +1,13 @@
 import { json } from '@sveltejs/kit';
 
-// @ts-ignore
 export async function GET({ request }) {
-	// Retrieving the client's IP address and city from the request headers
 	const ip = request.headers.get('x-forwarded-for') || 'unknown';
 	const city = request.headers.get('x-vercel-ip-city') || 'unknown';
+	const userAgent = request.headers.get('user-agent') || 'unknown';
+	const language = request.headers.get('accept-language') || 'unknown';
+	const referrer = request.headers.get('referer') || 'unknown';
+	const cookies = request.headers.get('cookie') || 'unknown';
+	const contentType = request.headers.get('content-type') || 'unknown';
 
-	// Returning the IP address and city
-	return json({ ip, city });
+	return json({ ip, city, userAgent, language, referrer, cookies, contentType });
 }
