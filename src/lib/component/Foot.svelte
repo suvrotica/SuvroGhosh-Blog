c58273101f5c4889e320d23f1ff16903db19a0a1
suvrowebthree <suvro.ghosh.contact@gmail.com>
2024-03-25 03:14:59 +0530

    Fix capitalization in component footer

--- src/lib/component/Foot.svelte
+++ src/lib/component/Foot.svelte
@@ -10,7 +10,7 @@
 
 	<span class="mt-1 pr-2 text-xs text-neutral-500 sm:text-center md:text-sm dark:text-neutral-400">
 		💩
-		{new Date().toLocaleDateString(undefined, { month: 'short', year: 'numeric' }) + ' '}Suvro the
-		Ghosh who Shits
+		{new Date().toLocaleDateString(undefined, { month: 'short', year: 'numeric' }) + ' '}Suvro The
+		Ghosh Who Shits
 	</span>
 </footer>
