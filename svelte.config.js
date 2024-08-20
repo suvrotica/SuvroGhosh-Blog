4efb867f5c9eb49f73f250167d30e6b07381ef53
suvrotica <suvrotica@gmail.com>
2024-08-20 15:10:56 +0530

    chore: Optimize Svelte configuration and add prerender error handling

--- svelte.config.js
+++ svelte.config.js
@@ -4,6 +4,9 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import sveltePreprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-vercel';
 
+
+
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', ...mdsvexConfig.extensions],
@@ -16,6 +19,16 @@ const config = {
 		mdsvex(mdsvexConfig)
 	],
 	kit: {
+		prerender: {
+			handleHttpError: ({ path, referrer, message }) => {
+			  // Ignore 404s for /latest-post
+			  if (path === '/latest-post' && message.includes('404')) {
+				return;
+			  }
+			  // Throw an error for other cases
+			  throw new Error(`${path} referred from ${referrer} failed to prerender: ${message}`);
+			}
+		  },
 		adapter: adapter({
 			runtime: 'edge',
 			regions: ['bom1', 'sin1']
