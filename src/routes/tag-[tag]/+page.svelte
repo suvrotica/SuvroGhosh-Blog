6a173746429aa45b3446b4006fd046e544ad8e82
suvrotica <suvrotica@gmail.com>
2024-08-20 14:55:27 +0530

    chore: Remove commented out code from layout.svelte

--- src/routes/tag-[tag]/+page.svelte
+++ src/routes/tag-[tag]/+page.svelte
@@ -1,18 +1,23 @@
 <script>
 	// @ts-nocheck
 	export let data;
+	// Add this function to safely capitalize the tag title
+    function capitalizeTag(tag) {
+        if (tag && typeof tag === 'string') {
+            return tag.charAt(0).toUpperCase() + tag.slice(1);
+        }
+        return '';
+    }
 
 	import { a_style } from '$lib/global';
 </script>
 
 <h1 class="first-letter:text-blue-500">
-	<span class="m-0 p-0"
-		>{data.tagTitle.substring(0, 1).toUpperCase()}{data.tagTitle.substring(1)}</span
-	>
+    <span class="m-0 p-0">{capitalizeTag(data?.tagTitle)}</span>
 </h1>
 <hr />
-{#each data?.posts as post}
-	<a href={`/slug-${encodeURIComponent(post.slug)}`} class={a_style}>
-		{post.title}
-	</a>
+{#each data?.posts ?? [] as post}
+    <a href={`/slug-${encodeURIComponent(post.slug)}`} class={a_style}>
+        {post.title}
+    </a>
 {/each}
